624cfb80c6209005e687e7f688f75f069a007a02
manishraza <manishrazasah22@gmail.com>
2023-08-11 08:05:14 +0545

    dictionary methods of task

---
+++ day 14/3_task.py
@@ -0,0 +1,15 @@
+# Count of the user input number in the list
+
+a = [5, 2, 3, 5, 3, 2, 3, 3, 1, 4]
+n = int(input("Pick a number "))
+count = a.count(n)
+print(f"Count of the number {n} in the list is", count)
+
+
+
+message = "Python + is + an + awesome + language"
+message_list = message.split(" + ")
+print(message_list)
+
+result = " ".join(message_list)
+print(result)
