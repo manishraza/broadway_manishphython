3c03cd456a7527b1f32d1b4df2d00a3cec4d06b2
manishraza <manishrazasah22@gmail.com>
2023-08-15 22:32:25 +0545

    map

--- day18/3_reduce_map.py
+++ day18/3_reduce_map.py
@@ -1,10 +1,16 @@
-# reduce
-nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# reduce()
+# reduce() is a built-in function that takes two parameters as an input. First parameter is a function
+# and second parameter is an iterable.
+# reduce() function returns one element from the final computation in the iterable.
 
 from functools import reduce
+nums = [1, 2, 3, 4, 5]
 def sum_all(x, y):
     return x + y
+
 result = reduce(sum_all, nums)
 print(nums)
 print(result)
-sum = lambda
+
+result = reduce(lambda x, y: x + y, nums)
+print(result)
--- day18/4_lambda.py
+++ day18/4_lambda.py
@@ -1,11 +1,13 @@
-# lambda in python are the elegant to create a on-online functions
-
+# lambda in python are the elegant way to create a one-liner functions
+# These functions do not have name. So, they are also called as anonymous function
 
 
 def addition(a, b):
     return a + b
-sum = lambda a, b: a + b
-print(sum(4, 5))
 
-is_even = lambda  x: x % 2 == 0
+
+summ = lambda a, b: a + b
+print(summ(4, 5))
+
+is_even = lambda x: x % 2 == 0
 print(is_even(4))
