8135a06e2d42f16bc66318df00518ecaa7f9062e
manishraza <manishrazasah22@gmail.com>
2023-08-24 19:53:02 +0545

    file handling

--- day 25/task.py
+++
@@ -1,8 +0,0 @@
-try:
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-
-    result = num1 + num2
-    print("The sum of {} and {} is {}".format(num1, num2, result))
-except ValueError:
-    print("Invalid input. Please enter valid numbers.")
--- day 25/task2.py
+++ day 25/task2.py
@@ -1,20 +1,7 @@
-def is_vowel(character):
-    try:
-        if len(character) != 1:
-            raise ValueError("Input should be a single character")
-
-        if character.lower() in 'aeiou':
-            return True
-        else:
-            return False
-    except ValueError as e:
-        print(f"Error: {e}")
-        return False
-
-
-# Test the function
-input_char = input("Enter a character: ")
-if is_vowel(input_char):
-    print(f"{input_char} is a vowel.")
-else:
-    print(f"{input_char} is not a vowel.")
+student = {"id": 1, "name": "Jane", "age": 30, "address": "KTM"}
+try:
+    key = input("Enter the key you want to access ")
+    data = student[key]
+    print(f"The {key} of the student is {data}")
+except KeyError:
+    print("Please enter a valid key")
